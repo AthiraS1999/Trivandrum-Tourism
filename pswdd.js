0e13566e698e003beace55b6a5c3fddd4bd3e3e1
AthiraS1999 <athirakarthu1999@gmail.com>
2024-09-22 10:42:25 +0530

    pswd

---
+++ pswdd.js
@@ -0,0 +1,18 @@
+var pswd=document.getElementById("pswd");
+function validate()
+{
+if(pswd.value.trim()=='')
+    {
+        alert("pswd cannot be empty");
+        return false;
+    }
+    else if(pswd.value.length <=5)
+    {
+        alert("pswd too short");
+        pswd.style.border="2px solid red";
+        return false;
+    }
+    else{
+        return true;
+    }
+}
